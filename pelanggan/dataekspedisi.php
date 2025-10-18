0db5110977940506de715fd05ae8fbce8ee775f3
ditftyyy <adityanusasyahputra@gmail.com>
2025-10-19 02:09:12 +0700

    membuat data ekspedisi php

---
+++ pelanggan/dataekspedisi.php
@@ -0,0 +1,4 @@
+<option value="">Pilih Ekspedisi</option>
+<option value="adit" nama="ADIT">ADIT</option>
+<option value="pos" nama="POS INDONESIA">POS INDONESIA</option>
+<option value="jne" nama="JNE">JNE</option>
